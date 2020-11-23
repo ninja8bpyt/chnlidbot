c15ac692a7bda012e0f34139442a3e0c97869256
Benchamxd <73537720+Benchamxd@users.noreply.github.com>
2020-11-23 11:42:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,9 @@ PLEASE DONT REMOVE THIS CREDITS
 */
 
 ob_start();
-define('API_KEY',"$BOT_TOKEN"); // put your token here
 ini_set("log_errors","off");
 function bot($method,$datas=[]){
-$url = "https://api.telegram.org/bot".API_KEY."/".$method;
+$url = "https://api.telegram.org/bot"$BOT_TOKEN"/".$method;
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
